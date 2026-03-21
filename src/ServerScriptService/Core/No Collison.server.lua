723655723af66bc44e540a0cf64aca0e4c6e4cf9
alexdiazwt <aadiaz1@buffs.wtamu.edu>
2026-03-20 19:23:25 -0500

    auto-checkpoint 2026-03-21T00:23:25.365584Z

--- src/ServerScriptService/Core/No Collison.server.lua
+++ src/ServerScriptService/Core/No Collison.server.lua
@@ -15,6 +15,15 @@ local function HandleCharacterAdded(character)
 	for _, part in ipairs(character:GetDescendants()) do
 		ChangeGroup(part)
 	end
+	-- P3-3: block jumping globally
+	local humanoid = character:FindFirstChildOfClass("Humanoid")
+	if humanoid then
+		humanoid.JumpHeight = 0
+	else
+		character.ChildAdded:Connect(function(obj)
+			if obj:IsA("Humanoid") then obj.JumpHeight = 0 end
+		end)
+	end
 end
 
 Players.PlayerAdded:Connect(function(player)
